9a3d2f0ecb309e0655c80b9a8011a188fbdf3e84
caipinfang <caipinfang@meituan.com>
2018-09-30 17:50:45 +0800

    ajax

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -3,7 +3,14 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.post('/post',(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// app.get('/',(req,res)=>{
+//   res.send('hello world');
+// });
+app.post('/ajax',(req,res)=>{
+  console.log("this is inside post func");
   console.log(req);
   res.send("test");
 });
